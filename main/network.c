da3d1fb9d98c8210c68431c5c6e0877d1ed36c1e
eric <epvuc@limpoc.com>
2021-02-06 17:57:33 -0800

    network.c now uses dest_ip  and dest_port globals from fumething-new.c, and there are two more levels of retry on ip socket failure

--- main/network.c
+++ main/network.c
@@ -14,12 +14,13 @@
 #include "lwip/err.h"
 #include "lwip/netdb.h"
 
-#define DEST_IP "10.0.0.1"
-#define DEST_PORT 31553
+/* dest_ip and dest_port are set in fumething_new.c */
+extern char *dest_ip;
+extern uint32_t dest_port;
 
 static struct sockaddr_in remote_addr;
 static int mysocket;
-extern xQueueHandle xUdpSendQueue;
+extern xQueueHandle xUdpSendQueue; // from fumething_new.c
 
 /* this just listens for queued messages from app_main() and sends them via udp to somewhere */
 void mynet_task(void *pvParameters) {
@@ -27,27 +28,33 @@ void mynet_task(void *pvParameters) {
   char udpbuf[128];
 
   while (1) { 
-    mysocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (mysocket < 0) {
-      ESP_LOGI("net", "socket failure");
-    } else { 
-      remote_addr.sin_family = AF_INET;
-      remote_addr.sin_port = htons(DEST_PORT);
-      remote_addr.sin_addr.s_addr = inet_addr(DEST_IP);
-
-      while (1) {
-	if (xQueueReceive(xUdpSendQueue, &udpbuf, (TickType_t)2)) {
-	  vTaskDelay(1); 
-	  len = sendto(mysocket, udpbuf, strlen(udpbuf), 0, (struct sockaddr *)&remote_addr, sizeof(remote_addr));
-	  if (len > 0) {
-	    // happytime
-	  } else {
-	    ESP_LOGI("net", "udp sendto failed");
-	    close(mysocket);
-	    break;
+    while (1) { 
+      mysocket = socket(AF_INET, SOCK_DGRAM, 0);
+      if (mysocket < 0) {
+	ESP_LOGI("net", "socket failure");
+	vTaskDelay(1000/portTICK_PERIOD_MS);
+	break; // at this point not much to do except wait and retry socket open.
+      } else { 
+	// dest_ip and dest_port can change via API
+	remote_addr.sin_family = AF_INET;
+	remote_addr.sin_addr.s_addr = inet_addr(dest_ip);
+	remote_addr.sin_port = htons(dest_port);
+	
+	while (1) {
+	  if (xQueueReceive(xUdpSendQueue, &udpbuf, (TickType_t)2)) {
+	    vTaskDelay(1); 
+	    len = sendto(mysocket, udpbuf, strlen(udpbuf), 0, (struct sockaddr *)&remote_addr, sizeof(remote_addr));
+	    if (len > 0) {
+	      // happytime
+	    } else {
+	      ESP_LOGI("net", "udp sendto failed");
+	      close(mysocket);
+	      break;
+	    }
 	  }
 	}
-      }
-    }    
+      }    
+    }
   }
+  // if we get here, everything's gone to shit.
 }
